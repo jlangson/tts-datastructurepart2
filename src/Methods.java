bc4fd03bd287a3640b6801919a9028adf9a1a1af
Jared Langson <langson.jared@gmail.com>
2020-11-17 22:48:36 -0500

    Wrote method to sum array

--- src/Methods.java
+++ src/Methods.java
@@ -1,2 +1,10 @@
 public class Methods {
+    //Write a program to sum all the values of a given Array in Java.
+    public static double sumDouble(double[] numbers){
+        double d = 0;
+        for(int i=0; i < numbers.length; i++){
+            d+=numbers[i];
+        }
+        return d;
+    }
 }
